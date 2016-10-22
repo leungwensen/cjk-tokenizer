558e1126c8973fc79dc6e58d54234190324d320a
leungwensen <leungwensen@gmail.com>
2016-10-22 08:29:58 +0800

    finishing english tokenizing

--- bin/cjk-tokenizer.js
+++ bin/cjk-tokenizer.js
@@ -5,3 +5,34 @@
  * @module cjk-tokenizer
  * @see module:index
  */
+const fs = require('fs');
+const commander = require('commander');
+const pkg = require('../package.json');
+const cjkTokenizer = require('../lib');
+
+function list(val) {
+    return val.split(',');
+}
+
+commander
+    .version(pkg.version)
+    .arguments('<filename>')
+    .option('-s, --stopWords <stopWords>', 'stop words', list)
+    .action((filename) => {
+        let content;
+        try {
+            content = fs.readFileSync(filename, 'utf-8');
+        } catch (e) {
+            console.error('Error: unable to read ' + filename + '.');
+            process.exit(1);
+        }
+        const tokens = cjkTokenizer.tokenize(content, {
+            stopWords: commander.stopWords || [],
+        });
+        console.log(JSON.stringify(tokens, null, 2));
+    });
+
+// execute client
+commander.parse(process.argv);
+
+if (process.argv.length === 2) commander.outputHelp();
